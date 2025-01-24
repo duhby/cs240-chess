e1c2d42a115d6d20906f7cb6d33ba99adeeca429
Josh <epikmemez@outlook.com>
2025-01-24 15:24:16 -0700

    chore: impl equals and hashCode

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +10,10 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPosition[][] position;
 
     public ChessBoard() {
-        
+        this.resetBoard();
     }
 
     /**
@@ -39,5 +43,22 @@ public class ChessBoard {
      */
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(position, that.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(position);
     }
 }
