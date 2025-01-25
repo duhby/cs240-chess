35b57d0fd3313bbfe8299c0d21a4a98e9b57d0ce
Josh <epikmemez@outlook.com>
2025-01-24 17:45:18 -0700

    feat: bishop moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -52,7 +53,129 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return switch(this.type) {
+            case KING -> kingMoves(board, myPosition);
+            case QUEEN -> queenMoves(board, myPosition);
+            case BISHOP -> bishopMoves(board, myPosition);
+            case KNIGHT -> knightMoves(board, myPosition);
+            case ROOK -> rookMoves(board, myPosition);
+            case PAWN -> pawnMoves(board, myPosition);
+        };
+    }
+
+    private Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
+    }
+
+    private Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = this.orthogonalMoves(board, myPosition);
+        moves.addAll(this.diagonalMoves(board, myPosition));
+        return moves;
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        return this.diagonalMoves(board, myPosition);
+    }
+
+    private Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
+        return this.orthogonalMoves(board, myPosition);
+    }
+
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private Collection<ChessMove> orthogonalMoves(ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    // TODO: repeat code less
+    private Collection<ChessMove> diagonalMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+        // Top right
+        for (int i = 1; i <= Math.min(8 - row, 8 - col); i++) {
+            ChessPosition newPosition = new ChessPosition(row + i, col + i);
+            if (this.isBlocked(board, newPosition)) {
+                break;
+            }
+            moves.add(new ChessMove(myPosition, newPosition, null));
+            if (this.canCapture(board, newPosition)) {
+                break;
+            }
+        }
+        // Bottom right
+        for (int i = 1; i <= Math.min(row - 1, 8 - col); i++) {
+            ChessPosition newPosition = new ChessPosition(row - i, col + i);
+            if (this.isBlocked(board, newPosition)) {
+                break;
+            }
+            moves.add(new ChessMove(myPosition, newPosition, null));
+            if (this.canCapture(board, newPosition)) {
+                break;
+            }
+        }
+        // Top left
+        for (int i = 1; i <= Math.min(8 - row, col - 1); i++) {
+            ChessPosition newPosition = new ChessPosition(row + i, col - i);
+            if (this.isBlocked(board, newPosition)) {
+                break;
+            }
+            moves.add(new ChessMove(myPosition, newPosition, null));
+            if (this.canCapture(board, newPosition)) {
+                break;
+            }
+        }
+        // Bottom left
+        for (int i = 1; i <= Math.min(row - 1, col - 1); i++) {
+            ChessPosition newPosition = new ChessPosition(row - i, col - i);
+            if (this.isBlocked(board, newPosition)) {
+                break;
+            }
+            moves.add(new ChessMove(myPosition, newPosition, null));
+            if (this.canCapture(board, newPosition)) {
+                break;
+            }
+        }
+
+        return moves;
+    }
+
+    private boolean isBlocked(ChessBoard board, ChessPosition newPosition) {
+        ChessPiece other = board.getPiece(newPosition);
+        if (other == null) {
+            return false;
+        }
+        return other.getTeamColor() == this.getTeamColor();
+    }
+
+    // Only implemented for non-pawn pieces, doesn't check if king will be in check
+    private boolean canCapture(ChessBoard board, ChessPosition newPosition) {
+        ChessPiece other = board.getPiece(newPosition);
+        if (other == null) {
+            return false;
+        }
+        return other.getTeamColor() != this.getTeamColor();
+    }
+
+    @Override
+    public String toString() {
+        return switch (this.type) {
+            case KING -> "K";
+            case QUEEN -> "Q";
+            case BISHOP -> "B";
+            case KNIGHT -> "N";
+            case ROOK -> "R";
+            // Pawn has no abbreviation unless it's capturing, but that logic needs to be implemented
+            // somewhere else instead anyway.
+            case PAWN -> "P";
+        };
     }
 
     @Override
