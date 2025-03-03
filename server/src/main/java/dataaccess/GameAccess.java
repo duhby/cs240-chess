de3ae13720bbbb16cfb5314c8c1146c9b2f01da8
Josh <epikmemez@outlook.com>
2025-03-03 16:18:24 -0700

    chore: add missing method

--- server/src/main/java/dataaccess/GameAccess.java
+++ server/src/main/java/dataaccess/GameAccess.java
@@ -2,12 +2,14 @@ package dataaccess;
 
 import java.util.Collection;
 
+import chess.ChessGame;
 import model.GameData;
 
 public interface GameAccess {
     GameData create(GameData data) throws DataAccessException;
     GameData get(int gameID) throws DataAccessException;
     GameData addPlayer(int gameID, String playerColor) throws DataAccessException;
+    GameData updatePosition(ChessGame game) throws DataAccessException;
     Collection<GameData> getAll() throws DataAccessException;
     void deleteAll() throws DataAccessException;
 }
