3b10b8590c901e802f691536a6736c5d6e1c30e9
Josh <epikmemez@outlook.com>
2025-03-03 16:09:06 -0700

    feat: data access interfaces

---
+++ server/src/main/java/dataaccess/AuthAccess.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthAccess {
+    AuthData create(AuthData data) throws DataAccessException;
+    AuthData get(String authToken) throws DataAccessException;
+    void delete(String authToken) throws DataAccessException;
+    void deleteAll() throws DataAccessException;
+}
---
+++ server/src/main/java/dataaccess/GameAccess.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import java.util.Collection;
+
+import model.GameData;
+
+public interface GameAccess {
+    GameData create(GameData data) throws DataAccessException;
+    GameData get(int gameID) throws DataAccessException;
+    GameData addPlayer(int gameID, String playerColor) throws DataAccessException;
+    Collection<GameData> getAll() throws DataAccessException;
+    void deleteAll() throws DataAccessException;
+}
