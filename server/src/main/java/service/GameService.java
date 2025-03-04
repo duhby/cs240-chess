e5ec18dab639b3ee7b7f847aa3b13305a05791e3
Josh <epikmemez@outlook.com>
2025-03-03 20:52:40 -0700

    feat: post /game

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,17 +1,33 @@
 package service;
 
-import dataaccess.AuthAccess;
-import dataaccess.UserAccess;
+import chess.ChessGame;
+import dataaccess.*;
 import exception.ResponseException;
 import model.AuthData;
-import model.UserData;
-import record.LoginRequest;
-import record.LoginResult;
-
-import java.util.Objects;
-import java.util.UUID;
+import model.GameData;
+import record.CreateGameRequest;
+import record.CreateGameResponse;
 
 public class GameService {
-    public GameService() {
+    private int nextID = 1;
+    private final AuthAccess authAccess;
+    private final GameAccess gameAccess;
+
+    public GameService(AuthAccess authAccess, GameAccess gameAccess) {
+        this.authAccess = authAccess;
+        this.gameAccess = gameAccess;
+    }
+
+    public CreateGameResponse create(CreateGameRequest data, String authToken) throws ResponseException {
+        authAccess.get(authToken);
+        if (data.gameName() == null) {
+            throw ResponseException.badRequest();
+        }
+        GameData game = gameAccess.create(new GameData(this.getNextID(), null, null, data.gameName(), new ChessGame()));
+        return new CreateGameResponse(game.gameID());
+    }
+
+    private int getNextID() {
+        return nextID++;
     }
 }
