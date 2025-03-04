7c81469fb008b12a9c16248e8beeb846d3eec040
Josh <epikmemez@outlook.com>
2025-03-03 17:35:50 -0700

    feat: login service

---
+++ server/src/main/java/record/LoginResult.java
@@ -0,0 +1,3 @@
+package record;
+
+public record LoginResult(String username, String authToken) {}
---
+++ server/src/main/java/service/AuthService.java
@@ -0,0 +1,28 @@
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import record.LoginRequest;
+import record.LoginResult;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class AuthService {
+    private final AuthAccess authAccess;
+    private final UserAccess userAccess;
+    public AuthService() {
+        authAccess = new AuthAccessMemory();
+        userAccess = new UserAccessMemory();
+    }
+
+    public LoginResult login(LoginRequest data) throws DataAccessException, UnauthorizedException {
+        UserData user = userAccess.get(data.username());
+        if (!Objects.equals(user.password(), data.password())) {
+            throw new UnauthorizedException();
+        }
+        AuthData auth = authAccess.create(new AuthData(UUID.randomUUID().toString(), user.username()));
+        return new LoginResult(user.username(), auth.authToken());
+    }
+}
