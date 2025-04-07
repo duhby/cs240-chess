33403eaafe3b58471aea27cf4b058d433860375d
Josh <epikmemez@outlook.com>
2025-04-07 17:28:25 -0600

    feat: server message types

---
+++ shared/src/main/java/websocket/messages/LoadGame.java
@@ -0,0 +1,12 @@
+package websocket.messages;
+
+import model.GameData;
+
+public class LoadGame extends ServerMessage {
+    GameData game;
+
+    public LoadGame(GameData game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+}
